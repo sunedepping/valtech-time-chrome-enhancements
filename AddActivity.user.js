f02ee819ba4271242e055c52a2cfd4708cf90d7d
Sune Jensen <sune@intraface.dk>
2013-04-24 14:20:55 +0200

    Rewrite of code to use jquery. Enhanced log

--- AddActivity.user.js
+++ AddActivity.user.js
@@ -1,17 +1,18 @@
 // ==UserScript==
 // @name       Add activity
 // @namespace  NaviWEB
-// @version    0.2
+// @version    0.3
 // @description  Makes it possible to add an activity in chrome browser
+// @match      http://time.valtech.dk/registration/reg_activity_edit.asp*
+// @require  https://ajax.googleapis.com/ajax/libs/jquery/1.8.2/jquery.min.js
 // @downloadURL https://raw.github.com/sunetjensen/valtech-time-chrome-enhancements/master/AddActivity.user.js
 // @updateURL https://raw.github.com/sunetjensen/valtech-time-chrome-enhancements/master/AddActivity.user.js
-// @match      http://time.valtech.dk/registration/reg_activity_edit.asp*
 // @copyright  2012+, Sune Jensen
 // ==/UserScript==
 
-function load(){
-    formcontent = document.getElementById("BottomDiv").outerHTML;
-    document.getElementById("BottomDiv").outerHTML = "<form action=\"_add_activity.asp\" id=\"CustomForm\" name=\"form1\" method=\"post\">" + formcontent + "</form>";
-}
-
-window.addEventListener('load', load);
+$(document).ready(function() {
+    console.log("Starting to modify HTML...");
+    console.log("Found div: " + $("#BottomDiv").length);
+    formcontent = $("#BottomDiv").html();
+    $("#BottomDiv").replaceWith("<form action=\"_add_activity.asp\" id=\"CustomForm\" name=\"form1\" method=\"post\"><div class=\"MainDiv\" id=\"BottomDiv\">" + formcontent + "</div></form>");
+});
